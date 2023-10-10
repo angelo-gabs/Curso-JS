8a9ff33cc95619fdb4723f44ba9fcdc1802af496
Angelo Gabriel <127778596+angelo-gabs@users.noreply.github.com>
2023-10-09 23:45:18 -0300

    Add files via upload

---
+++ 34.estruturas de repetição/index.js
@@ -0,0 +1,29 @@
+/*
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+
+/*
+for (let i = 0; i<=5;  i++) {
+ console.log(`linha ${i}`)
+}
+*/
+
+/*
+for (let i = 0; i <= 500; i += 10) { // com o += ele vai pular com relação ao valor dado logo em seguida. Sendo assim ele foi pulando de dez em dez
+    console.log (`Lista ${i}`);
+} 
+
+
+for (let i = 0; i <= 10; i++) {
+   const par = i % 2 === 0 ? 'par' : 'impar';
+   console.log(i, par)
+}
+*/
+const frutas = ['Maça', 'Pêra', 'Uva'];
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
